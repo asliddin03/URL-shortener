b54e4782963009be32595a96985da94fd46443b0
asliddin03 <boltaev.ak@phystech.edu>
2024-07-06 10:48:48 +0300

    first commit

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,16 +1,15 @@
 package config
 
 import (
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"os"
 	"time"
-
-	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-require:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
@@ -18,8 +17,6 @@ type HTTPServer struct {
 	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
-	User        string        `yaml:"user" env-required:"true"`
-	Password    string        `yaml:"password" env-required:"true" env:"HTTP_SERVER_PASSWORD"`
 }
 
 func MustLoad() *Config {
@@ -29,8 +26,9 @@ func MustLoad() *Config {
 	}
 
 	// check if file exists
+
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
-		log.Fatalf("config file does not exist: %s", configPath)
+		log.Fatalf("config file %s does not exist", configPath)
 	}
 
 	var cfg Config
